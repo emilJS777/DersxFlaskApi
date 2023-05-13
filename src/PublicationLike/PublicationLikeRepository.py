bd520300d444a64f17f38ae99f120407b289d6da
emiljs777 <e.pargevich@mail.ru>
2023-05-13 16:48:31 +0400

    commit

--- src/PublicationLike/PublicationLikeRepository.py
+++ src/PublicationLike/PublicationLikeRepository.py
@@ -1,12 +1,9 @@
 from .IPublicationLikeRepo import IPublicationLikeRepo
 from .PublicationLikeModel import PublicationLike
 from flask import g
-from cachetools import cached, TTLCache
-from src import app
 
 
 class PublicationLikeRepository(IPublicationLikeRepo):
-    cache = TTLCache(maxsize=app.config['CACHE_SIZE'], ttl=app.config['CACHE_TTL'])
 
     def create(self, body: dict):
         publication_like: PublicationLike = PublicationLike()
@@ -17,7 +14,6 @@ class PublicationLikeRepository(IPublicationLikeRepo):
     def delete(self, publication_like: PublicationLike):
         publication_like.delete_db()
 
-    @cached(cache)
     def get(self, publication_like_id: int or None = None, publication_id: int or None = None, user_id: int or None = None) -> PublicationLike:
         publication_like: PublicationLike = PublicationLike.query.filter(
             PublicationLike.id == publication_like_id if publication_like_id else PublicationLike.id.isnot(None),
