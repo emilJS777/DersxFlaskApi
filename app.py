40d2d04dd82783ca1b95fad5c8c6eea2aef4b4af
emiljs777 <e.pargevich@mail.ru>
2023-05-19 12:19:56 +0400

    commit

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from src import app
+from OpenSSL import SSL
+
+ssl_context = SSL.Context(SSL.SSLv23_METHOD)
+ssl_context.use_privatekey_file('/etc/ssl/certs/privkey.pem')
+ssl_context.use_certificate_chain_file('/etc/ssl/certs/fullchain.pem')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(ssl_context=ssl_context, port=5000)
