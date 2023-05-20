e37e52f51ab4b8776ec96797a07fe34cc85c4dc5
emiljs777 <e.pargevich@mail.ru>
2023-05-20 19:46:39 +0400

    validator

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from src import app
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True, ssl_context="adhoc")
+    app.run(ssl_context=('/etc/letsencrypt/live/skillx.am/cert.pem', '/etc/letsencrypt/live/skillx.am/privkey.pem'))
